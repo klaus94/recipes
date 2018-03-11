04498cda6a10a6045cc1a0ce5443d602bf2b58a2
Dominik Olwig <supermagic999@web.de>
2018-03-11 03:00:03 +0100

    made all things working together

--- recipes-client/src/app/components/imageUploadComponent/imageUpload.component.ts
+++ recipes-client/src/app/components/imageUploadComponent/imageUpload.component.ts
@@ -13,4 +13,14 @@ export class ImageUploadComponent
     {
         this.imageChanged = new EventEmitter<File>();
     }
+
+    fileChangeEvent(fileInput: any)
+    {
+        console.log(fileInput);
+        if (fileInput.target.files)
+        {
+            console.log(fileInput.target.files[0]);
+            this.imageChanged.next(fileInput.target.files[0]);
+        }
+    }
 }
